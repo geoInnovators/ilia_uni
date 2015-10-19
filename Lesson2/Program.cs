91fc34895f93570615d34ca27a08759e1ffafc89
avto <avtoruxadze@gmail.com>
2015-10-19 13:02:42 +0400

    aa

---
+++ Lesson2/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2
+{
+    class Program
+    {
+        static void print(string s)
+        {
+            Console.WriteLine(s);
+        }
+
+        static void Main(string[] args)
+        {
+            //TestParse();
+            TestString();
+
+            Console.ReadKey();
+        }
+
+
+        static void TestParse()
+        {
+            String s = Console.ReadLine();
+
+            int a ;   //  = int.Parse(s);
+            
+            bool res = int.TryParse(s, out a );
+            if (res == true)
+            {
+                Console.WriteLine(a + 1);
+            }
+            else
+            {
+                Console.WriteLine("araswori formati");
+            }
+            
+            
+        }
+
+        static void TestChar()
+        {
+            char c = 'ა';
+            Console.WriteLine(char.IsLower('A'));   // false
+            Console.WriteLine(char.IsLower('a'));   // true
+            Console.WriteLine(char.IsUpper('A'));   // true
+            Console.WriteLine(char.IsUpper('a'));   // true
+
+        }
+
+        static void TestString()
+        {
+            string s;
+            s = "avto";  // s = "avto"
+            s = new string('A', 10  );  // AAAAAAAAAA
+            char[] mas = new[] {'a', 'v', 't', 'o'};
+            s = "avto"; // s = "avto"
+
+            char a = s[3];   //  a = 'o'
+
+            s = "avto rukhadze";
+            bool o = s.Contains("rukhadze");  // true
+
+            bool c = s.Contains('l');  // false
+            bool d = s.Contains("l");  // false
+
+            o = s.StartsWith("avto");  // true
+            o = s.StartsWith("rukhadze");  // false
+
+            o = s.EndsWith("rukhadze");  // true
+            o = s.EndsWith("avto");  // false
+            s = "avto rukhadze rukhadze";
+            int index = s.IndexOf("rukhadze"); // index = 5
+            int endIndex = index + "rukhadze".Length;
+            index = s.LastIndexOf("rukhadze");  // index = 13
+            index = s.IndexOf("giorgi"); // index = -1;
+
+            s = "avto";
+            // concat
+            string s2 = s + " giorgi";  // s2 = "avto giorgi"
+            // substring
+            string s3 = s2.Substring(5, 6); // giorgi
+            string s4 = s2.Substring(5); // giorgi
+            string s5 = "avtogiorgi".Insert(4, " ");
+            string s6 = "avto giorgi".Remove(4);  // avto
+            string s7 = "avto giorgi".Remove(4, 1);  // avtogiorgi
+
+
+            Console.WriteLine(s7);
+        }
+
+
+    }
+}
---
+++ Lesson2/Student.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2
+{
+    //refference anu mimtitebliani tipi
+    class Student
+    {
+        #region fields
+        public int Age;
+        public string Name;
+        public string LastName;
+        public int Course;
+        public int[] Credits;
+        #endregion
+
+        #region methods
+        public string DisplayName() {
+            return Name + " " + LastName + " " + SummeryCredits;
+        }
+        #endregion
+
+        #region constructors
+
+        public Student()
+        {
+            //
+        }
+        public Student(string name, string lastname)
+        {
+            Name = name;
+            LastName = lastname;
+        }
+        #endregion
+
+        #region properties
+        public int cred;
+        public int SummeryCredits
+        {
+            get
+            {
+                int res=0;
+                for (int i = 0; i < Credits.Length; i++)
+                    res += Credits[i];
+                return res;
+            }
+
+            set 
+            {
+                cred = value;
+            }
+        }
+
+        #endregion
+
+        #region static members
+        public static int StudentCount;
+
+        public static void AddStudent(Student stud)
+        {
+            StudentCount++;
+        }
+        #endregion
+    }
+    //value type. aqvs yvelaferi rac klass ar aqvs memkvidreoba da ar aris mimtitebeli.
+    struct Lesson
+    {
+        public days day;
+        public int time;
+    }
+
+    //enum
+    enum days
+    { 
+        sun,
+        mon,
+        tues
+    }
+}

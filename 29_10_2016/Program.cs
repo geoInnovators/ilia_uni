ddc157f8a12e2f0d325b448bcea1ee1d0ccbac64
avtoruxadze <avtoruxadze@DESKTOP-C3B7Q4E>
2016-10-30 15:28:03 +0400

    ...

--- 29_10_2016/Program.cs
+++ 29_10_2016/Program.cs
@@ -4,83 +4,82 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CommonClasses;
-using student = CommonClasses.Student;
+
 
 namespace _29_10_2016
 {
-    struct Point
-    {
-        int X;
-        int Y;
-    }
 
     class Program
     {
+        static void TestParams(Student a, out int b)
+        {
+            a.Name = "giorgi";
+            b = 20;
+        }
         static void Main(string[] args)
         {
             ReadNumber();
-            ReferenceType();
-
-            Student s = new Student("aa", "bb");
-            s.Credits = new int[] { 8, 9, 10  };
-            
-            Console.WriteLine(s.IsCreditGood);
-            s.IsCreditGood = true;
-            Console.WriteLine(s.IsCreditGood);
-
-
-            Student b = new Student("cc", "dd");
-            Student.Students = new Student[]
-            {
-                new Student("cc", "dd", 18),
-                new Student("cc", "dd", 20)
-            };
-            Console.WriteLine(Student.StudentsAge(20));
-
-            // enum gamozaxeba
-            var test = new Student("cc", "dd", 20);
-            test.StudentType = StudentTypes.Master;
-
-            var test2 = new Student("cc", "dd", 20);
-
             Console.ReadKey();
+
+            Student student1 = new Student("a", "b");
+            int a = 15;
+            TestParams(student1, out a);
+            Console.WriteLine(student1.Name); // 
+            Console.WriteLine(a);  //  20
+
+
+            TestReference();
+            Console.ReadLine();
+
+            Student student = new Student("a", "b");
+            student.PersonalId = "sdgwegew";
+            Console.WriteLine(student.IsStudentGeorgianProrety);
+            student.IsStudentGeorgianProrety = "no";
+
+            student.Credits = new int[] { 10, 5, 15 };
+            Console.WriteLine(student.IsStudentGood  ); // true false
+
+            Student.Students = new Student[] {
+                new Student("a","b",25),
+                new Student("a","b",20),
+                new Student("a","b"),
+            };
+            Console.WriteLine(Student.GetStudentsCountByAge(20));
+            Console.ReadLine();
         }
 
-        static void ReferenceType()
+        static void TestReference()
         {
-            var student1 = new Student("cc", "dd", 20);
-            var student2 = student1;
-            student2.Name = "avto";
-            Console.WriteLine(student1.Name); // ras dagvibechdavs
-
-            int a;
-            a = 5;
-            int b = a;
-            b = 7;
-            Console.WriteLine(a); // ras dagvibechdavs
+            Student s1 = new Student("avto", "sdg");
+            Student s2 = s1;
+            s2 = new Student("dato", "sdg");
+            s2 = null;
+            int? a = null;
+            Console.WriteLine(s1.Name);
         }
 
-        static void Synonyms()
+        static void TestEnum()
         {
-            String s1 = "aaa";
-            string s2 = "bbbb";
-
-            int a = 4;
-            Int32 b = 5;
-
-            student s;
-            Student ss;
+            Student s1 = new Student("avto", "sdg");
+            s1.StudentType = StudentTypes.Master;
         }
-
+        
         static void ReadNumber()
         {
-            string s = Console.ReadLine(); // 27
-            int a; //int.Parse(s);
+            string s = Console.ReadLine();  // s = "234"
+            Console.WriteLine(s + 5);
+            int a;
             if( int.TryParse(s, out a) == true)
+            {
                 Console.WriteLine(a + 5);
+            }
             else
-                Console.WriteLine("sheiyvanet ricxvi!");
-
+            {
+                Console.WriteLine("invalid number!");
+            }
+            
         }
+
+
     }
 }
--- CommonClasses/Student.cs
+++ CommonClasses/Student.cs
@@ -6,19 +6,25 @@ using System.Threading.Tasks;
 
 namespace CommonClasses
 {
+    public enum StudentTypes
+    {
+        Bachelor,
+        Master,
+        Phd
+    }
+
     public class Student
     {
         #region fields
         public string Name;
-        public string LastName;
-        public int Age;
-        public string PersonalId;
-        public StudentTypes StudentType;
+        public string LastName; 
+        public int Age; 
+        public string PersonalId; 
 
         private string _isGeorgian;
 
         public int[] Credits;
-
+        public StudentTypes StudentType;
         #endregion
 
         #region constructors
@@ -46,6 +52,12 @@ namespace CommonClasses
         {
             return Name + " " + LastName;
         }
+
+        public void TestThis(int Age)
+        {
+            
+            Age = 34;
+        }
         #endregion
 
         #region properties
@@ -63,59 +75,32 @@ namespace CommonClasses
             }
         }
 
-        private bool _isCreditsGood;
-        public bool IsCreditGood
+        public bool IsStudentGood
         {
             get
             {
-                if (_isCreditsGood == true)
-                    return _isCreditsGood;
-
-                int minCreditsForB = 30;
-                int minCreditsForM = 20;
-                int minCreditsForP = 10;
-
                 int sum = 0;
                 foreach(int credit in Credits)
                 {
                     sum += credit;
                 }
-
-                if( StudentType  == StudentTypes.Bachelor)
-                    return (sum >= minCreditsForB);
-                if (StudentType == StudentTypes.Master)
-                    return (sum >= minCreditsForM);
-                if (StudentType == StudentTypes.Phd)
-                    return (sum >= minCreditsForP);
-                return false;
-
-            }
-            set
-            {
-                _isCreditsGood = value;
+                return sum >= 50;
             }
         }
+
         #endregion
 
-        #region static fields
+        #region static member
         public static Student[] Students;
-        public static int StudentsAge(int age)
+        public static int GetStudentsCountByAge(int age)
         {
             int count = 0;
-            foreach (var s in Students)
+            foreach(Student s in Students)
             {
-                if (s.Age == age)
-                    count++;
+                if (s.Age == age) count++;
             }
             return count;
         }
         #endregion
-    }
-
-    public enum StudentTypes
-    {
-        Bachelor,
-        Master,
-        Phd
     }
 }

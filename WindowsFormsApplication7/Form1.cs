8caac3db38cd75c6cda508846cb1f5fcdc825496
avtoruxadze <avtoruxadze@DESKTOP-C3B7Q4E>
2017-04-03 22:28:02 +0400

    ff

---
+++ WindowsFormsApplication7/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            int x = r.Next(this.Width);
+            int y = r.Next(this.Height);
+            label1.Location = new Point(x, y);
+        }
+    }
+}

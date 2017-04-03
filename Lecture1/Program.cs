15b89429eaf8ad41add404504a2872e73533fac7
avtoruxadze <avtoruxadze@DESKTOP-C3B7Q4E>
2017-04-03 22:38:51 +0400

    ttt

---
+++ Lecture1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using testNamespace;
+namespace Lecture1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 9;
+            Test(out a);
+            Console.WriteLine(a); // ras dabechdavs
+            Console.ReadLine();
+        }
+
+        static void Test(out int a)
+        {
+            a = 0;
+            a = a + 1;
+        }
+    }
+}

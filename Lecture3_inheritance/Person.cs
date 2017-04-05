ebed8e2061201ed9214dbccf46f5030d4fc1dd82
avtoruxadze <avtoruxadze@DESKTOP-C3B7Q4E>
2017-04-05 10:10:03 +0400

    ...

---
+++ Lecture3_inheritance/Person.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture3_inheritance
+{
+    abstract class Person
+    {
+        public string Name;
+        public string LastName;
+        public DateTime BirthDate;
+        public Person(string name, string lastname, DateTime birthDate)
+        {
+            Name = name;
+            LastName = lastname;
+            BirthDate = birthDate;
+        }
+
+        public abstract string GetFullName(); // abstractuli metodi
+
+        // TODO: saxlshi
+        public int GetPersonAge()
+        {
+            return 0;
+        }
+    }
+
+    class Student : Person
+    {
+        public override string GetFullName()
+        {
+            return "Student - " + Name + " " + LastName;
+        }
+
+        public decimal Payment;
+        public Student(string name, string lastname, DateTime birthDate)
+            : base(name,lastname, birthDate)
+        {
+        }
+    }
+    class Lecturer : Person
+    {
+        public override string GetFullName()
+        {
+            return "Lecturer - " + Name + " " + LastName;
+        }
+
+        public decimal Salary;
+        public Lecturer(string name, string lastname, DateTime birthDate)
+            : base(name,lastname, birthDate)
+        {
+        }
+    }
+}
---
+++ Lecture3_inheritance/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture3_inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(1996, 02, 21);
+            Student st = new Student("dato", "rukhadze", dt);
+            Lecturer l = new Lecturer("aaa", "bbb", new DateTime(1956, 02, 03));
+            Console.WriteLine(st.GetFullName());
+            Console.WriteLine(l.GetFullName());
+
+            st.Payment = 34.23m;
+            Person p = st;
+            Console.WriteLine(p.Name);  // dato
+            Console.WriteLine(st.Payment);  // 34.23 
+            Student a = (Student)p; // tipis dayvana
+            // Lecturer al = (Lecturer)p; // runtime error
+            bool b = p is Lecturer; // true
+            Console.WriteLine(b);
+            Console.ReadLine();
+            // Person p = new Person("dato", "rukhadze", dt);
+
+        }
+    }
+
+
+}
